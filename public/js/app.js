b4f3b7fc77334442ba70fb007885f1f9cd188d53
Jan Christian Bernabe <spramp@users.noreply.github.com>
2018-02-21 22:23:28 -0600

    Blotter test

--- public/js/app.js
+++ public/js/app.js
@@ -1,4 +1,18 @@
-// Contact form script //
+// Blotter.js //
 
-var contactform = document.getElementById('contactform');
-contactform.setAttribute('actions', '//formspree.io/' + 'janchristianbernabe' + '@' + 'gmail' + '.' + 'com');
+var text = new Blotter.Text("Jan Christian Bernabe", {
+  family : "'Source Sans Pro', sans-serif",
+  size : 2rem,
+  fill : "rgba(215, 96, 177, 1)"
+});
+
+var material = new Blotter.Material();
+
+var blotter = new Blotter(material, {
+  texts : text
+});
+
+var elem = document.getElementById("plain-text");
+var scope = blotter.forText(text);
+
+scope.appendTo(elem);
