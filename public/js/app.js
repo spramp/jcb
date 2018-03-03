dd17aa6e24c85c911c7c70361b144e846d8a1903
Jan Christian Bernabe <spramp@users.noreply.github.com>
2018-03-02 23:18:30 -0600

    Test 1.3

--- public/js/app.js
+++ public/js/app.js
@@ -1,26 +1,25 @@
-jQuery.noConflict();
-
 (function( $ ) {
 
 var lastUrl = "http://ws.audioscrobbler.com/2.0/?method=user.getrecenttracks&user=twentysixlicks&api_key=336d6c1e30852f655412888f276a0112&limit=1&format=json";
 
 $.ajax({
-     url: "lastUrl",
-    },
-
+    url: "lastUrl",
     type: "GET",
     dataType : "json",
     async: true,
-})
+  });
+  
   .done(function( json ) {
      $( "<div class="song" ).text( json.title ).appendTo( "body" );
-  })
+  });
+
   .fail(function( xhr, status, errorThrown ) {
     alert( "Sorry, there was a problem!" );
     console.log( "Error: " + errorThrown );
     console.log( "Status: " + status );
     console.dir( xhr );
-  })
+  });
+
   .always(function( xhr, status ) {
     alert( "The request is complete!" );
   });
